6fba593ca926643181bfd3d647792d86a60cd266
vfomin2904 <vova-fomin97@mail.ru>
2021-08-24 23:23:50 +0300

    linquibase

---
+++ shop-database/src/main/java/ru/geekbrains/persist/model/Brand.java
@@ -0,0 +1,51 @@
+package ru.geekbrains.persist.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "brand")
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "brand")
+    private Set<Product> products;
+
+    public Brand(){
+    }
+
+    public Brand(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+}
